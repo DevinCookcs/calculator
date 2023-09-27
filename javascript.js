43c93b8f674f0f0fa2de29cabbc99a57ac5f2feb
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-09-27 15:42:10 -0600

    added event listeners to html references and added function names

--- javascript.js
+++ javascript.js
@@ -1,26 +1,26 @@
-function add(x, y){
+function add(x, y) {
     var result = x + y;
     return result;
 }
 
-function subtract(x, y){
+function subtract(x, y) {
     var result = x - y;
     return result;
 }
 
-function multiply(x, y){
+function multiply(x, y) {
     var result = x * y;
     return result;
 }
 
-function divide(x, y){
+function divide(x, y) {
     var result = x / y;
     return result;
 }
 
-function operate(x, operand, y){
+function operate(x, y, operand) {
     var result;
-    switch (operand){
+    switch (operand) {
         case "+":
             result = add(x, y);
             break;
@@ -37,6 +37,60 @@ function operate(x, operand, y){
     return result;
 }
 
-var x;
-var y;
-var operand;
+//initialize variables
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldReset = false;
+
+//get references to html elements
+const lastOperationScreen = document.querySelector(".last");
+const currentOperationScreen = document.querySelector(".current");
+const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
+const numberButton = document.querySelectorAll(["data-number"]);
+const operatorButton = document.querySelectorAll(["data-operator"]);
+const pointButton = document.getElementById("point-btn");
+const equalsButton = document.getElementById("equals-btn");
+
+//add event listeners to elements
+clearButton.addEventListener("click", clear);
+deleteButton.addEventListener("click", deleteNumber);
+pointButton.addEventListener("click", appendPoint);
+equalsButton.addEventListener("click", evaluate);
+
+numberButton.forEach((button) =>
+    button.addEventListener("click", () => 
+        appendNumber(button.textContent))
+)
+
+operatorButton.forEach((button) =>
+    button.addEventListener("click", () => 
+        setOperation(button.textContent))
+)
+
+
+
+function clear() {
+
+}
+
+function deleteNumber() {
+
+}
+
+function appendPoint() {
+
+}
+
+function evaluate() {
+
+}
+
+function appendNumber() {
+
+}
+
+function setOperation() {
+
+}
