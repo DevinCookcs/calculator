c8aa7ea09a1b010ac6f391e7b3cd114e8b233b47
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-09-27 12:50:05 -0600

    created operate function and variables to store input

--- javascript.js
+++ javascript.js
@@ -17,3 +17,26 @@ function divide(x, y){
     var result = x / y;
     return result;
 }
+
+function operate(x, operand, y){
+    var result;
+    switch (operand){
+        case "+":
+            result = add(x, y);
+            break;
+        case "-":
+            result = subtract(x, y);
+            break;
+        case "*":
+            result = multiply(x, y);
+            break;
+        case "/":
+            result = divide(x, y);
+            break;
+    }
+    return result;
+}
+
+var x;
+var y;
+var operand;
